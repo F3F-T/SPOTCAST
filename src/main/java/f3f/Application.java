18939d51228c8a947dbe74b2b3197c78cb48e4ae
임동혁 <idh1007@naver.com>
2022-12-05 16:10:01 +0900

    [ADD] 임동혁 branch

--- src/main/java/f3f/Application.java
+++ src/main/java/f3f/Application.java
@@ -6,12 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-<<<<<<<<< Temporary merge branch 1
 
-
-=========
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
->>>>>>>>> Temporary merge branch 2
 }
