e1146af60ec1a05e4978d31c6382f52fc359bab9
임동혁 <idh1007@naver.com>
2022-12-16 00:03:04 +0900

    [ADD] Auth 관련 controller 추가

--- src/main/java/f3f/domain/user/api/MemberController.java
+++ src/main/java/f3f/domain/user/api/MemberController.java
@@ -5,10 +5,7 @@ import f3f.domain.user.dto.MemberDTO;
 import f3f.domain.user.dto.TokenDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static f3f.domain.user.dto.MemberDTO.*;
 import static f3f.domain.user.dto.TokenDTO.*;
@@ -18,18 +15,11 @@ import static f3f.domain.user.dto.TokenDTO.*;
 @RequiredArgsConstructor
 public class MemberController {
     private final MemberService memberService;
-    @PostMapping("/signup")
-    public ResponseEntity<Long> signup(@RequestBody MemberSaveRequestDto memberRequestDto) {
-        return ResponseEntity.ok(memberService.saveMember(memberRequestDto));
+
+
+    @GetMapping("/{memberId}")
+    public ResponseEntity<MemberInfoResponseDto> findMemberInfoById(@PathVariable Long memberId) {
+        return ResponseEntity.ok(memberService.findMyPageInfo(memberId));
     }
 
-    @PostMapping("/login")
-    public ResponseEntity<TokenResponseDTO> login(@RequestBody MemberLoginRequestDto loginRequestDto) {
-        return ResponseEntity.ok(memberService.login(loginRequestDto));
-    }
-
-    @PostMapping("/reissue")
-    public ResponseEntity<TokenResponseDTO> reissue(@RequestBody TokenRequestDTO tokenRequestDto) {
-        return ResponseEntity.ok(memberService.reissue(tokenRequestDto));
-    }
 }
