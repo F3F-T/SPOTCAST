2dd119592c91ba1593f8a1c6327f328585928cf3
임동혁 <idh1007@naver.com>
2022-12-16 03:55:55 +0900

    [REFACTOR] jwt 및 spring security 관련 exception response 처리

--- src/main/java/f3f/global/jwt/JwtAccessDeniedHandler.java
+++ src/main/java/f3f/global/jwt/JwtAccessDeniedHandler.java
@@ -1,6 +1,7 @@
 package f3f.global.jwt;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import f3f.global.exception.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
@@ -10,11 +11,18 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.springframework.http.HttpStatus.FORBIDDEN;
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 
 @Component
 @Slf4j
 public class JwtAccessDeniedHandler implements AccessDeniedHandler {
 
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response,
@@ -22,17 +30,17 @@ public class JwtAccessDeniedHandler implements AccessDeniedHandler {
 
         // 필요한 권한이 없이 접근하려 할때 403
         // ADMIN 페이지에 USER 가 접근하려고 할 때 or 로그인이 필요한 페이지에 접근하려고할 때
-        log.info("accessDeniedException : " + accessDeniedException);
-        response.sendError(HttpServletResponse.SC_FORBIDDEN);
+        sendResponse(response, accessDeniedException);
+
     }
-//    private void setResponse(HttpServletResponse response, ExceptionCode exceptionCode) throws IOException {
-//        response.setContentType("application/json;charset=UTF-8");
-//        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-//
-//        JSONObject responseJson = new JSONObject();
-//        responseJson.put("message", exceptionCode.getMessage());
-//        responseJson.put("code", exceptionCode.getCode());
-//
-//        response.getWriter().print(responseJson);
-//    }
+
+    private void sendResponse(HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException {
+        String result = objectMapper.writeValueAsString(new ErrorResponse(FORBIDDEN, accessDeniedException.getMessage()));
+
+
+        response.setContentType("application/json");
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write(result);
+
+    }
 }
--- src/main/java/f3f/global/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/f3f/global/jwt/JwtAuthenticationEntryPoint.java
@@ -1,6 +1,7 @@
 package f3f.global.jwt;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import f3f.global.exception.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
@@ -9,18 +10,34 @@ import org.springframework.stereotype.Component;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 
 @Component
 @Slf4j
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
 
+    private final ObjectMapper objectMapper = new ObjectMapper();
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
         // 유효한 자격증명을 제공하지 않고 접근하려 할때 401
         // 즉 토큰이 없거나 유효하지 않은 경우
-        log.info("authException : " + authException);
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+        sendResponse(response, authException);
+
+    }
+
+    private void sendResponse(HttpServletResponse response, AuthenticationException authException) throws IOException {
+
+        String result = objectMapper.writeValueAsString(new ErrorResponse(UNAUTHORIZED, authException.getMessage()));
+
+
+        response.setContentType("application/json");
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write(result);
+
     }
 
 }
