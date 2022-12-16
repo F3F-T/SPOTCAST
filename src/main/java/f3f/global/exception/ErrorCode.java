d3dc712bd4c9c509639955c643476dea396a3f5c
ryudongjae <rdj10149@gmail.com>
2022-12-16 15:43:28 +0900

    [ADD] API 예외 응답 체계 공통화

--- src/main/java/f3f/global/exception/ErrorCode.java
+++ src/main/java/f3f/global/exception/ErrorCode.java
@@ -7,8 +7,12 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
+    //이메일 중복
     DUPLICATION_EMAIL(HttpStatus.BAD_REQUEST,"이메일 중복"),
+
+    //auth 불일치
     UNAUTHORIZATION(HttpStatus.UNAUTHORIZED,"authority 불일치"),
+    //토큰 없음
     JWT_ACCESSDENIED(HttpStatus.UNAUTHORIZED,"토큰 없음");
 
     private final HttpStatus httpStatus;
