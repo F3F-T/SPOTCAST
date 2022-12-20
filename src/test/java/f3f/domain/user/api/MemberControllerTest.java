90d8b8cc84f1d01ab98393e6d5b8123a2de54a78
임동혁 <idh1007@naver.com>
2022-12-21 00:14:25 +0900

    [ADD] 멤버 컨트롤러 세팅

--- src/test/java/f3f/domain/user/api/MemberControllerTest.java
+++ src/test/java/f3f/domain/user/api/MemberControllerTest.java
@@ -1,4 +1,46 @@
 package f3f.domain.user.api;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import f3f.domain.user.application.MemberService;
+import f3f.domain.user.dao.MemberRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.test.web.servlet.setup.SharedHttpSessionConfigurer.sharedHttpSession;
+
+@WebMvcTest({MemberController.class, MemberAuthController.class})
+@MockBean(JpaMetamodelMappingContext.class)
+@ExtendWith({RestDocumentationExtension.class, SpringExtension.class})
+티
 public class MemberControllerTest {
+    @MockBean
+    private MemberService memberService;
+
+    @MockBean
+    private MemberRepository memberRepository;
+
+    private MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+
+    @BeforeEach
+    public void setup(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentationContextProvider) {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .apply(sharedHttpSession())
+                .apply(documentationConfiguration(restDocumentationContextProvider))
+                .build();
+    }
 }
