17a460a5bc87b093f9652b61cb5c776a31779d30
ryudongjae <rdj10149@gmail.com>
2022-12-21 22:25:39 +0900

    [ADD] 지원하기 기능 1차 구현

---
+++ src/main/java/f3f/domain/teamApply/exception/NotfoundException.java
@@ -0,0 +1,11 @@
+package f3f.domain.teamApply.exception;
+
+public class NotfoundException extends IllegalArgumentException {
+
+    public NotfoundException() {
+    }
+
+    public NotfoundException(String s) {
+        super(s);
+    }
+}
