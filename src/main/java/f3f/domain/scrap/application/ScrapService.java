7f8c7f65fd9e664a2bb59658df65fc8124ae89bd
임동혁 <idh1007@naver.com>
2023-01-03 09:33:19 +0900

    [REFACTOR] scrap save 서비스 로직 변경

--- src/main/java/f3f/domain/scrap/application/ScrapService.java
+++ src/main/java/f3f/domain/scrap/application/ScrapService.java
@@ -1,5 +1,6 @@
 package f3f.domain.scrap.application;
 
+import f3f.domain.board.domain.Board;
 import f3f.domain.scrap.dao.ScrapBoardRepository;
 import f3f.domain.scrap.dao.ScrapRepository;
 import f3f.domain.scrap.domain.Scrap;
@@ -45,9 +46,16 @@ public class ScrapService {
         Member findMember = memberRepository.findById(memberId)
                 .orElseThrow(() -> new MemberNotFoundException("존재하지 않는 사용자입니다."));
 
-        boolean nameExists = findMember.getScrapList().contains(request.getName());
-        if(nameExists){
-            throw new DuplicateScrapNameException("스크랩 이름이 중복되었습니다.");
+
+        List<ScrapDTO.ScrapInfoDTO> scrapInfoDTOList = findMember.getScrapList().stream()
+                .map(Scrap::toScrapInfoDTO).collect(Collectors.toList());
+
+        for (ScrapDTO.ScrapInfoDTO scrapInfoDTO : scrapInfoDTOList) {
+            if(scrapInfoDTO.getName() == request.getName())
+            {
+                System.out.println("scrapInfoDTO = " + scrapInfoDTO.getName());
+                throw new DuplicateScrapNameException("스크랩 이름이 중복되었습니다.");
+            }
         }
 
         Scrap scrap = scrapRepository.save(request.toEntity(findMember));
