b815310df4882ca6081342deec6db20a449f9053
ryudongjae <rdj10149@gmail.com>
2023-01-03 23:03:15 +0900

    [ADD] Response 응답체계 완$$$$$$$$$$$$$성

--- src/main/java/f3f/global/response/GlobalExceptionHandler.java
+++ src/main/java/f3f/global/response/GlobalExceptionHandler.java
@@ -1,46 +1,58 @@
 package f3f.global.response;
 
-import f3f.domain.user.exception.DuplicateEmailException;
-import f3f.domain.user.exception.MemberNotFoundException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import static f3f.global.constants.ResponseConstants.DUPLICATION_EMAIL;
+import javax.validation.ConstraintViolationException;
 
 
 @Slf4j
-@RestControllerAdvice
-public class GlobalExceptionHandler {
+@RestControllerAdvice(annotations = {RestController.class})
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(DuplicateEmailException.class)
-    protected final ResponseEntity<String> handleDuplicateEmailException(
-            DuplicateEmailException ex, WebRequest request) {
-        log.debug("Duplicate email :: {}, detection time = {}",request.getDescription(false));
-        return DUPLICATION_EMAIL;
+    @ExceptionHandler
+    public ResponseEntity<Object> validation(ConstraintViolationException e, WebRequest request) {
+        return handleExceptionInternal(e, ErrorCode.VALIDATION_ERROR, request);
     }
 
-    @ExceptionHandler(MemberNotFoundException.class)
-    protected final ResponseEntity<String> handleMemberNotFoundException(
-            MemberNotFoundException ex, WebRequest request) {
-        log.debug("member not found :: {}, detection time = {}",ex.getMessage());
-        return new ResponseEntity<>(
-                ex.getMessage(), HttpStatus.CONFLICT);
+    @ExceptionHandler
+    public ResponseEntity<Object> general(GeneralException e, WebRequest request) {
+        return handleExceptionInternal(e, e.getErrorCode(), request);
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Object handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-        String errorMessage = e.getBindingResult()
-                .getAllErrors()
-                .get(0)
-                .getDefaultMessage();
+    @ExceptionHandler
+    public ResponseEntity<Object> exception(Exception e, WebRequest request) {
+        return handleExceptionInternal(e, ErrorCode.INTERNAL_ERROR, request);
+    }
 
-        return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body,
+                                                             HttpHeaders headers, HttpStatus status, WebRequest request) {
+        return handleExceptionInternal(ex, ErrorCode.valueOf(status), headers, status, request);
     }
 
 
+    private ResponseEntity<Object> handleExceptionInternal(Exception e, ErrorCode errorCode,
+                                                           WebRequest request) {
+        return handleExceptionInternal(e, errorCode, HttpHeaders.EMPTY, errorCode.getHttpStatus(),
+                request);
+    }
+
+    private ResponseEntity<Object> handleExceptionInternal(Exception e, ErrorCode errorCode,
+                                                           HttpHeaders headers, HttpStatus status, WebRequest request) {
+        return super.handleExceptionInternal(
+                e,
+                ExceptionResponseDTO.of(errorCode, errorCode.getMessage(e)),
+                headers,
+                status,
+                request
+        );
+    }
 }
--- src/main/java/f3f/global/response/ResultDataResponseDTO.java
+++ src/main/java/f3f/global/response/ResultDataResponseDTO.java
@@ -1,11 +1,30 @@
 package f3f.global.response;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.ToString;
+public class ResultDataResponseDTO<T> extends ResponseDTO  {
 
+    private final T data;
 
-public class ResultDataResponseDTO {
+    private ResultDataResponseDTO(T data) {
+        super(true, ErrorCode.OK.getCode(), ErrorCode.OK.getMessage());
+        this.data = data;
+    }
+
+    private ResultDataResponseDTO(T data, String message) {
+        super(true, ErrorCode.OK.getCode(), message);
+        this.data = data;
+    }
+
+    public static <T> ResultDataResponseDTO<T> of(T data) {
+        return new ResultDataResponseDTO<>(data);
+    }
+
+    public static <T> ResultDataResponseDTO<T> of(T data, String message) {
+        return new ResultDataResponseDTO<>(data, message);
+    }
+
+    public static <T> ResultDataResponseDTO<T> empty() {
+        return new ResultDataResponseDTO<>(null);
+    }
 }
 
 
