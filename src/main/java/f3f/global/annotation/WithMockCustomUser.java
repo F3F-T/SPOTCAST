75c618a3a949e3177b16b82967ba0131744c79bf
임동혁 <idh1007@naver.com>
2023-01-05 16:01:11 +0900

    [ADD] controller 테스트에서 securityContext 활용을 위한 annotation

---
+++ src/main/java/f3f/global/annotation/WithMockCustomUser.java
@@ -0,0 +1,17 @@
+package f3f.global.annotation;
+
+
+import f3f.global.util.WithMockCustomUserSecurityContextFactory;
+import org.springframework.security.test.context.support.WithSecurityContext;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+@WithSecurityContext(factory = WithMockCustomUserSecurityContextFactory.class)
+public @interface WithMockCustomUser {
+    String userName() default "1";
+
+    String role() default "ROLE_USER";
+
+}
