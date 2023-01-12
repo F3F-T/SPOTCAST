2298baa4e8d1701c22225bd19bac967b0628b2be
ryudongjae <rdj10149@gmail.com>
2023-01-12 18:16:43 +0900

    [REFACTOR] 보드 타입 추가

--- src/main/java/f3f/domain/publicModel/BoardType.java
+++ src/main/java/f3f/domain/publicModel/BoardType.java
@@ -3,7 +3,9 @@ package f3f.domain.publicModel;
 public enum BoardType {
 
     RECRUIT(1,"구인"),
-    JOB_SEARCH(2,"구직"),
+    CASTING_AUDITION(2,"캐스팅/오디션"),
+    COMMUNITY(4,"커뮤니티"),
+    ARTWORK(5,"작업물"),
     GENERAL(3,"일반");
 
     private long id;
