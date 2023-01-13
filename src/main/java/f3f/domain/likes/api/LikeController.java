6b28b3e10fd9a238d68b5cf6f3f46b7eb56efb31
ryudongjae <rdj10149@gmail.com>
2023-01-13 22:41:24 +0900

    [REFACTOR] 좋아요 ,댓글 조회 API 수정

--- src/main/java/f3f/domain/likes/api/LikeController.java
+++ src/main/java/f3f/domain/likes/api/LikeController.java
@@ -2,12 +2,15 @@ package f3f.domain.likes.api;
 
 import f3f.domain.likes.application.LikeService;
 import f3f.domain.likes.dto.LikeDTO;
+import f3f.global.response.ResultDataResponseDTO;
 import f3f.global.util.SecurityUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -25,15 +28,15 @@ public class LikeController {
 
     /*게시글 좋아요 삭제*/
     @DeleteMapping("/board/{boardId}/unlike")
-    public ResponseEntity deleteLike(@PathVariable Long boardId) {
+    public ResultDataResponseDTO<Long> deleteLike(@PathVariable Long boardId) {
         Long memberId = SecurityUtil.getCurrentMemberId();
         likeService.deleteLike(boardId, memberId);
-        return ResponseEntity.ok(boardId);
+        return ResultDataResponseDTO.of(boardId);
     }
 
-    @GetMapping("/board/{boardId}")
-    public ResponseEntity getLikeList(@PathVariable Long boardId) {
-        Long memberId = SecurityUtil.getCurrentMemberId();
-        return ResponseEntity.ok(boardId);
+    @GetMapping("/board/{boardId}/likeList")
+    public ResultDataResponseDTO<List<LikeDTO.LikeInfo>> getLikeList(@PathVariable Long boardId) {
+        List<LikeDTO.LikeInfo> listListByBoardId = likeService.getListListByBoardId(boardId);
+        return ResultDataResponseDTO.of(listListByBoardId);
     }
 }
