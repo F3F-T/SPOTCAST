8eaaa5fe02532f20e712e3a890da8531c2c5dce0
ryudongjae <rdj10149@gmail.com>
2023-01-17 23:03:11 +0900

    [REFACTOR] 이미지 저장 , 삭제 , 조회  API 구현 (S3 연동 보류)

--- src/main/java/f3f/domain/BoardImage/api/BoardImageController.java
+++ src/main/java/f3f/domain/BoardImage/api/BoardImageController.java
@@ -2,11 +2,13 @@ package f3f.domain.BoardImage.api;
 
 import f3f.domain.BoardImage.application.BoardImageService;
 import f3f.domain.BoardImage.domain.BoardImage;
+import f3f.domain.BoardImage.dto.BoardImageDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -16,18 +18,18 @@ public class BoardImageController {
 
     private final BoardImageService boardImageService;
 
-    @PostMapping()
-    public BoardImage saveBoardImage(@PathVariable long boardId, @RequestBody List<MultipartFile> imageList){
-        return null;
+    @PostMapping(value = "/board/boardImage/{boardId}")
+    public String saveBoardImage(@PathVariable long boardId, @RequestBody List<MultipartFile> imageList) throws IOException {
+        return boardImageService.saveBoardImage(boardId,imageList);
     }
 
-    @DeleteMapping
-    public BoardImage deleteBoardImage(){
-        return null;
+    @DeleteMapping(value = "/board/boardImage")
+    public BoardImage deleteBoardImage(@RequestBody BoardImage boardImage){
+        return boardImageService.deleteBoardImage(boardImage);
     }
 
-    @GetMapping
-    public List<BoardImage> getBoardImageList(){
-        return null;
+    @GetMapping(value = "/board/boardImage/{boardId}")
+    public List<BoardImageDTO.BoardImageInfo> getBoardImageList(@PathVariable long boardId){
+        return boardImageService.getBoardImageList(boardId);
     }
 }
