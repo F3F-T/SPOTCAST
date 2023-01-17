1a857abfec0705b85e64863d98612bd7e86ee5f4
ryudongjae <rdj10149@gmail.com>
2023-01-17 22:23:53 +0900

    [REFACTOR] 게시글 이미지 저장

--- src/main/java/f3f/domain/BoardImage/application/BoardImageService.java
+++ src/main/java/f3f/domain/BoardImage/application/BoardImageService.java
@@ -3,10 +3,14 @@ package f3f.domain.BoardImage.application;
 import f3f.domain.BoardImage.dao.BoardImageRepository;
 import f3f.domain.BoardImage.domain.BoardImage;
 import f3f.domain.board.dao.BoardRepository;
+import f3f.domain.board.domain.Board;
+import f3f.infra.aws.S3.S3Uploader;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @Service
@@ -15,13 +19,23 @@ public class BoardImageService {
 
     private final BoardRepository boardRepository;
     private final BoardImageRepository boardImageRepository;
+    private final S3Uploader s3Uploader;
 
     @Transactional
-    public BoardImage saveBoardImage(long boardId , List<BoardImage> boardImage){
+    public String  saveBoardImage(long boardId , List<MultipartFile> boardImageList) throws IOException {
+        Board board = boardRepository.findById(boardId).orElseThrow();
+        for (MultipartFile multipartFile : boardImageList) {
+            String imagePath = s3Uploader.upload(multipartFile, "임시 ");
+            BoardImage boardImage = BoardImage.builder()
+                    .board(board)
+                    .s3Url(imagePath)
+                    .build();
+            boardImageRepository.save(boardImage);
+        }
         //1차로 boardId 로 게시글을 찾아온다.
         //찾아왔으면 보드 객체를 이미지에다가 넣어주고
         //이미지 객체 S3랑 DB 에 업데이트 DB 에 넣는값은 S3경로
-        return null;
+        return "임시";
     }
 
     @Transactional
