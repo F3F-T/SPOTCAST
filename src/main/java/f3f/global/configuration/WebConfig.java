ef48aadd39650d844b019909d8408478618d40cf
임동혁 <idh1007@naver.com>
2023-01-19 14:18:43 +0900

    [REFACTOR] cors 에러 수정

--- src/main/java/f3f/global/configuration/WebConfig.java
+++ src/main/java/f3f/global/configuration/WebConfig.java
@@ -10,9 +10,8 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000","http://localhost:3001")
-                .allowedMethods("GET", "POST", "PATCH", "DELETE")
+                .allowedMethods("*")
                 .allowedHeaders("*")
-                .allowCredentials(true)
-                .maxAge(3000);
+                .allowCredentials(true);
     }
 }
