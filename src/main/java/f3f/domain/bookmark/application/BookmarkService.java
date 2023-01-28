a697b5cbe97a49f39054ad9dd5a43ab2240f7147
임동혁 <idh1007@naver.com>
2023-01-28 23:31:24 +0900

    [REFACTOR] bookmark 추가 삭제 시 성공 실패 return

--- src/main/java/f3f/domain/bookmark/application/BookmarkService.java
+++ src/main/java/f3f/domain/bookmark/application/BookmarkService.java
@@ -37,41 +37,44 @@ public class BookmarkService extends BaseTimeEntity {
 
     // 북마크 요청
     @Transactional
-    public void followRequest(BookmarkDTO.BookmarkRequestDto requestDto, Long currentMemberId){
-        if(!requestDto.getFollowerId().equals(currentMemberId)){
+    public Boolean followRequest(BookmarkDTO.BookmarkRequestDto requestDto, Long currentMemberId) {
+        if (!requestDto.getFollowerId().equals(currentMemberId)) {
             throw new GeneralException(ErrorCode.MISMATCH_FOLLOW, "본인만 팔로우를 할 수 있습니다.");
         }
-        if(requestDto.getFollowerId().equals(requestDto.getFollowingId())){
+        if (requestDto.getFollowerId().equals(requestDto.getFollowingId())) {
             throw new GeneralException(ErrorCode.MISMATCH_FOLLOW, "본인을 팔로우할 수 없습니다.");
         }
-        if(!memberRepository.existsById(requestDto.getFollowerId())){
+        if (!memberRepository.existsById(requestDto.getFollowerId())) {
             throw new GeneralException(ErrorCode.NOTFOUND_MEMBER, "존재하지 않는 사용자의 요청입니다.");
         }
-        if(!memberRepository.existsById(requestDto.getFollowingId())){
+        if (!memberRepository.existsById(requestDto.getFollowingId())) {
             throw new GeneralException(ErrorCode.NOTFOUND_MEMBER, "존재하지 않는 사용자입니다.");
         }
-        bookmarkRepository.saveFollowRequest(requestDto.getFollowerId(), requestDto.getFollowingId());
+        Integer result = bookmarkRepository.saveFollowRequest(requestDto.getFollowerId(), requestDto.getFollowingId());
+
+        return result == 1;
 
     }
 
     //북마크 취소 요청
     @Transactional
-    public void followCancel(BookmarkDTO.BookmarkRequestDto requestDto){
+    public Boolean followCancel(BookmarkDTO.BookmarkRequestDto requestDto) {
 
-        bookmarkRepository.deleteByFollowerIdAndFollowingId(requestDto.getFollowerId(),requestDto.getFollowingId());
+        Integer result = bookmarkRepository.deleteByFollowerIdAndFollowingId(requestDto.getFollowerId(), requestDto.getFollowingId());
+        return result == 1;
     }
 
     //나를 팔로우하는 사람들 리스트
     @Transactional
-    public Slice<BookmarkDTO.BookmarkListResponseDto> getFollowerList(Long memberId, Pageable pageable){
+    public Slice<BookmarkDTO.BookmarkListResponseDto> getFollowerList(Long memberId, Pageable pageable) {
         Slice<BookmarkDTO.BookmarkListResponseDto> followerList = searchBookmarkRepository.getFollowerListByMemberId(memberId, pageable);
         return followerList;
     }
 
     //내가 팔로우하는 사람들 리스트
     @Transactional
-    public Slice<BookmarkDTO.BookmarkListResponseDto> getFollowingList(Long memberId,Pageable pageable){
-        Slice<BookmarkDTO.BookmarkListResponseDto> followingList = searchBookmarkRepository.getFollowingListByMemberId(memberId,pageable);
+    public Slice<BookmarkDTO.BookmarkListResponseDto> getFollowingList(Long memberId, Pageable pageable) {
+        Slice<BookmarkDTO.BookmarkListResponseDto> followingList = searchBookmarkRepository.getFollowingListByMemberId(memberId, pageable);
         return followingList;
     }
 }
