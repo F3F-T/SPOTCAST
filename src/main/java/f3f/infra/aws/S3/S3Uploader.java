66ef93afb428612aebd44243b058ef70ddcbaf3c
임동혁 <idh1007@naver.com>
2023-02-05 18:35:29 +0900

    [REFACTOR] aws 오류 로그 제거

--- src/main/java/f3f/infra/aws/S3/S3Uploader.java
+++ src/main/java/f3f/infra/aws/S3/S3Uploader.java
@@ -1,7 +1,13 @@
 package f3f.infra.aws.S3;
 
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import f3f.global.response.ErrorCode;
+import f3f.global.response.GeneralException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -15,45 +21,42 @@ import java.util.Optional;
 @Service
 public class S3Uploader {
 
-//    private final AmazonS3Client amazonS3Client;
+    private final AmazonS3Client amazonS3Client;
 
-//    @Value("${cloud.aws.s3.bucket}")
-//    private String bucket;
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
 
     // MultipartFile을 전달받아 File로 전환한 후 S3에 업로드
     public String upload(MultipartFile multipartFile, String dirName) throws IOException {
-//        File uploadFile = convert(multipartFile)
-//                .orElseThrow(() -> new IllegalArgumentException("MultipartFile -> File 전환 실패"));
-//        return upload(uploadFile, dirName);
-//    }
-//
-//    public String deleteImage(String bucket, String key){
-//        try{
-//            amazonS3Client.deleteObject(bucket,key);
-//            return "SUCCESS";
-//        }catch (Exception e){
-//            throw new GeneralException(ErrorCode.S3_ERROR,"S3 파일 삭제");
-        //}
-        return null;
+        File uploadFile = convert(multipartFile)
+                .orElseThrow(() -> new IllegalArgumentException("MultipartFile -> File 전환 실패"));
+        return upload(uploadFile, dirName);
+    }
 
+    public String deleteImage(String bucket, String key){
+        try{
+            amazonS3Client.deleteObject(bucket,key);
+            return "SUCCESS";
+        }catch (Exception e) {
+            throw new GeneralException(ErrorCode.S3_ERROR, "S3 파일 삭제");
+        }
     }
 
     private String upload(File uploadFile, String dirName) {
-//        String fileName = dirName + "/" + uploadFile.getName();
-//        String uploadImageUrl = putS3(uploadFile, fileName);
-//
-//        removeNewFile(uploadFile);  // 로컬에 생성된 File 삭제 (MultipartFile -> File 전환 하며 로컬에 파일 생성됨)
-//
-//        return uploadImageUrl;      // 업로드된 파일의 S3 URL 주소 반환
-//    }
-//
-//    private String putS3(File uploadFile, String fileName) {
-//        amazonS3Client.putObject(
-//                new PutObjectRequest(bucket, fileName, uploadFile)
-//                        .withCannedAcl(CannedAccessControlList.PublicRead)	// PublicRead 권한으로 업로드 됨
-//        );
-//        return amazonS3Client.getUrl(bucket, fileName).toString();
-        return null;
+        String fileName = dirName + "/" + uploadFile.getName();
+        String uploadImageUrl = putS3(uploadFile, fileName);
+
+        removeNewFile(uploadFile);  // 로컬에 생성된 File 삭제 (MultipartFile -> File 전환 하며 로컬에 파일 생성됨)
+
+        return uploadImageUrl;      // 업로드된 파일의 S3 URL 주소 반환
+    }
+
+    private String putS3(File uploadFile, String fileName) {
+        amazonS3Client.putObject(
+                new PutObjectRequest(bucket, fileName, uploadFile)
+                        .withCannedAcl(CannedAccessControlList.PublicRead)	// PublicRead 권한으로 업로드 됨
+        );
+        return amazonS3Client.getUrl(bucket, fileName).toString();
     }
 
     private void removeNewFile(File targetFile) {
