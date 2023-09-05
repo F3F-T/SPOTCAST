51dc5f765ca42fc816be3a2e07e4de2e53e04716
임동혁 <idh1007@naver.com>
2023-09-05 20:44:01 +0900

    [REFACTOR]transaction annotation 수정

--- src/main/java/f3f/domain/user/application/UserDetailService.java
+++ src/main/java/f3f/domain/user/application/UserDetailService.java
@@ -10,18 +10,18 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
 import java.util.Collections;
 
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class UserDetailService implements UserDetailsService {
 
     private final MemberRepository memberRepository;
 
     @Override
-    @Transactional
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
         Member findMember = memberRepository.findByEmail(username);
