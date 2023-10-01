a8c769b4fe71a638f8e1374c08e2320aca932013
임동혁 <idh1007@naver.com>
2023-10-01 18:39:10 +0900

    [REFACTOR] 임시 ACCESS TOKEN 시간 변경

--- src/main/java/f3f/global/constants/JwtConstants.java
+++ src/main/java/f3f/global/constants/JwtConstants.java
@@ -10,7 +10,9 @@ public class JwtConstants {
 
     public static final int ACCESS_TOKEN_COOKIE_EXPIRE_TIME = 60 * 60 * 24 * 7 ;    //7일
 
-    public static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 30 ;            // 30분
+//    public static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 30 ;            // 30분
+    public static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 ;            // 1일
+
     public static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 7;  // 7일
 
     public static final long REFRESH_TOKEN_EXPIRE_TIME_SECOND =  60 * 60 * 24 * 7;  // 7일
